95b53694e1e4d0206cd61c48c2ef1574e9b18e4f
root <alexnewman42@gmail.com>
2024-04-30 09:01:45 -0400

    Start setPlayerCards function

--- script.js
+++ script.js
@@ -182,4 +182,15 @@ teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr.map(
+    ({ name, position, number, isCaptain, nickname }) => {
+      `
+        <div class="player-card">
+        <h2>${name}</h2>
+        </div>
+      `;
+    }
+  );
+};
 
